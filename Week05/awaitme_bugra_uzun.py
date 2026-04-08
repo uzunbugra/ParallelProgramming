9c1da8de6dfa10b0ad6339ed54e7ed1b963ca111
Buğra Uzun <112521231+uzunbugra@users.noreply.github.com>
2026-04-08 10:03:29 +0300

    Add awaitme decorator for asynchronous functions

---
+++ Week05/awaitme_bugra_uzun.py
@@ -0,0 +1,8 @@
+import asyncio
+from functools import wraps
+
+def awaitme(func):
+    @wraps(func)
+    async def wrapper(*args, **kwargs):
+        return func(*args, **kwargs)
+    return wrapper
